396cded9ed1873bdff45ec3796bc5306599158c7
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-08-24 13:01:39 +0600

    config file added

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import app from "./app";
+import config from "./config";
 import { prisma } from "./lib/prisma";
 
-const PORT = 5000;
+const PORT = config.port;
 
 async function main() {
   try {
     await prisma.$connect();
     console.log("Connected to the database successfully");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port: ${PORT}`);
-    });
+    if (config.node_env !== "production") {
+      app.listen(PORT, () => {
+        console.log(`Server is running on port: ${PORT}`);
+      });
+    }
   } catch (error) {
     console.log("Error starting the server:", error);
     prisma.$disconnect();
